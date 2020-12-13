10a067e65d4e5171e92ba79aee767885d925f6b5
Lilith-In-Starlight <veromora0712@gmail.com>
2020-12-12 20:24:26 -0500

    Created game scene

--- Scripts/Menu.gd
+++ Scripts/Menu.gd
@@ -1,16 +1,9 @@
 extends Control
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+func _on_Play_pressed():
+	get_tree().change_scene_to(preload("res://Scenes/Moloch.tscn"))
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
+func _on_Exit_pressed():
 	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
