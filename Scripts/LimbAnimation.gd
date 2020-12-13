473741060078c7b32afb837ff663db1f18f0c09f
Lilith-In-Starlight <veromora0712@gmail.com>
2020-12-12 21:36:36 -0500

    Fixed her thing also

--- Scripts/LimbAnimation.gd
+++ Scripts/LimbAnimation.gd
@@ -14,19 +14,4 @@ func _process(delta):
 		go_to = go_to.normalized() * 20
 		go_dist = 20
 	
-	# TODO: I hate this way to do this. If I don't fix this in the future 
-	# that means something went awfully wrong with my life
-	if go_dist >= 19.0:
-		frame = move_toward(frame, 0, 1)
-	elif go_dist >= 18.0:
-		frame = move_toward(frame, 1, 1)
-	elif go_dist >= 17.0:
-		frame = move_toward(frame, 2, 1)
-	elif go_dist >= 16.0:
-		frame = move_toward(frame, 3, 1)
-	elif go_dist >= 15.0:
-		frame = move_toward(frame, 4, 1)
-	elif go_dist >= 14.0:
-		frame = move_toward(frame, 5, 1)
-	else:
-		frame = move_toward(frame, 6, 1)
+	frame = move_toward(frame, 19 - clamp(int(go_dist), 19, 13), 1)
